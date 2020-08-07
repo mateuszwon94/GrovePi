688b89d47c7d88fbea1d1ad4be6d3c0a1c84ae13
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2020-08-07 20:21:21 +0200

    Add first basic sample

---
+++ GrovePi/Samples/Basic.cs
@@ -0,0 +1,17 @@
+﻿using System.Device.I2c;
+
+using Iot.Device.GrovePiDevice;
+
+using static System.Console;
+
+namespace GrovePiApplication.Samples {
+    class Basic {
+        static void Main(string[] args) {
+            I2cConnectionSettings i2CConnectionSettings = new I2cConnectionSettings(1, GrovePi.DefaultI2cAddress);
+            GrovePi grovePi = new GrovePi(I2cDevice.Create(i2CConnectionSettings));
+            WriteLine($"Manufacturer: {grovePi.GrovePiInfo.Manufacturer}");
+            WriteLine($"Board: {grovePi.GrovePiInfo.Board}");
+            WriteLine($"Firmware version: {grovePi.GrovePiInfo.SoftwareVersion}");
+        }
+    }
+}
